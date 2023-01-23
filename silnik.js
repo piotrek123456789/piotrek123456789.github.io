89b5072178a23f5d734868c5c3638e41dd6b4a42
piotrek123456789 <119850583+piotrek123456789@users.noreply.github.com>
2023-01-23 10:05:17 +0100

    Add files via upload

---
+++ silnik.js
@@ -0,0 +1,36 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+// initialize kaboom context
+kaboom();
+
+loadSprite("tlo","swieta.png")
+loadSprite("balwan","balwan.png")
+
+
+// add a piece of text at position (120, 80)
+add([
+    text("hello"),
+    pos(120, 80),
+]);
+
+add([
+    sprite("tlo"),
+    pos(0,0)
+])
+
+const balwan = add([
+    sprite("balwan"),
+    pos(10, 190)
+])
+
+onUpdate(()=>{
+
+    if(balwan.pos.x<570)
+    
+    balwan.pos.x=balwan.pos.x+1
+
+    else  if(balwan.pos.y>100) balwan.pos.y=balwan.pos.y-1
+
+
+})
+
